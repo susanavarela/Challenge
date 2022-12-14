6e7d262221820e9bda46a5e6a658dfed3b3c6469
SUSANAVARELA <sudava10@gmail.com>
2022-12-14 17:52:57 -0300

    project creation

---
+++ src/test/java/com/challenge/Accenture/AccentureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.Accenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
