98ae88a759ede83f4771f67e574bd11a55e6ea78
SUSANAVARELA <sudava10@gmail.com>
2022-12-16 13:35:52 -0300

    created swagger

--- src/main/java/com/challenge/Accenture/controllers/BusinessController.java
+++ src/main/java/com/challenge/Accenture/controllers/BusinessController.java
@@ -1,7 +1,5 @@
 package com.challenge.Accenture.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.challenge.Accenture.dto.AlbumDto;
+import com.challenge.Accenture.dto.CommentDto;
 import com.challenge.Accenture.dto.PermissionDto;
 import com.challenge.Accenture.dto.PhotoDto;
 import com.challenge.Accenture.dto.UserDto;
@@ -22,6 +21,12 @@ import com.challenge.Accenture.service.CommentsService;
 import com.challenge.Accenture.service.PermissionsService;
 import com.challenge.Accenture.service.PhotosService;
 import com.challenge.Accenture.service.UsersService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 
 
@@ -44,45 +49,91 @@ public class BusinessController {
 	@Autowired
 	private CommentsService commentsService ;
 	
-	//retorna todos los usuarios
+	@Operation(summary = "Trae todos los usuarios")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "200", description = "Se trae a los usuarios con exito", 
+	    content = { @Content(mediaType = "application/json", 
+	    schema = @Schema(implementation = UserDto.class)) }),
+	@ApiResponse(responseCode = "404", description = "Los usuarios no fueron encontrados en el servicio web", content = @Content)})
 	@GetMapping("/users")
-	public ResponseEntity<List<UserDto>> findAllUsers(){
-		return ResponseEntity.ok().body(usersService.getUsers());
+	public ResponseEntity<Object> findAllUsers(){
+		try {
+			return ResponseEntity.ok().body(usersService.getUsers());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
 	}
 	
-	//retorna todos los albums
+	@Operation(summary = "Trae todos los albums")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "200", description = "Se trae a los albums con exito", 
+	    content = { @Content(mediaType = "application/json", 
+	    schema = @Schema(implementation = AlbumDto.class)) }),
+	@ApiResponse(responseCode = "404", description = "Los albums no fueron encontrados en el servicio web", content = @Content)})
 	@GetMapping("/albums")
-	public ResponseEntity<List<AlbumDto>> findAllAlbums(){
-		return ResponseEntity.ok().body(albumsService.getAlbums());
+	public ResponseEntity<Object> findAllAlbums(){
+		try {
+			return ResponseEntity.ok().body(albumsService.getAlbums());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}	
 	}
 	
-	//retorna todos los fotos de un album
+	@Operation(summary = "Trae todas las fotos de un album")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "200", description = "Se trae a las fotos con exito", 
+	    content = { @Content(mediaType = "application/json", 
+	    schema = @Schema(implementation = PhotoDto.class)) }),
+	@ApiResponse(responseCode = "404", description = "Las fotos no fueron encontradas en el servicio web", content = @Content)})
 	@GetMapping("/photos/{idalbum}")
-	public ResponseEntity<List<PhotoDto>> findAllPhotos(@PathVariable("idalbum") int idAlbum){
-		return ResponseEntity.ok().body(photosService.getPhotos(idAlbum));
+	public ResponseEntity<Object> findAllPhotos(@PathVariable("idalbum") int idAlbum){
+		try {
+			return ResponseEntity.ok().body(photosService.getPhotos(idAlbum));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}		
 	}
 	
-	//retorna todas las fotos de un usuario
+	@Operation(summary = "Trae todas las fotos de un usuario")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "200", description = "Se trae a las fotos con exito", 
+	    content = { @Content(mediaType = "application/json", 
+	    schema = @Schema(implementation = PhotoDto.class)) }),
+	@ApiResponse(responseCode = "404", description = "Las fotos no fueron encontradas en el servicio web", content = @Content)})
 	@GetMapping("/photos/user={iduser}")
 	public ResponseEntity<Object> findPhotosByUser(@PathVariable("iduser") int idUser){
 		try {	
-			return ResponseEntity.status(HttpStatus.CREATED).body(photosService.getPhotosByUser(idUser));
+			return ResponseEntity.ok().body(photosService.getPhotosByUser(idUser));
 		}catch(Exception e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error en la busqueda de datos, " + e.getMessage());
 		}
 	}
 	
-	//retorna todos los albums de un usuario enviando su id
+	@Operation(summary = "Trae todos los albums de un usuario enviando su id")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "200", description = "Se trae todos los albums con exito", 
+	    content = { @Content(mediaType = "application/json", 
+	    schema = @Schema(implementation = AlbumDto.class)) }),
+	@ApiResponse(responseCode = "404", description = "Los albums no fueron encontradas en el servicio web", content = @Content)})
 	@GetMapping("/userandalbums/{id}")
-	public ResponseEntity<List<AlbumDto>> userAndAlbums(@PathVariable("id") int id){
+	public ResponseEntity<Object> userAndAlbums(@PathVariable("id") int id){
+		try {
 		return ResponseEntity.ok().body(albumsService.getUserAndAlbums(id));
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error en la busqueda de datos, " + e.getMessage());
+		}
 	}
 	
-	//retorna lista de comentarios en base a postId
+	@Operation(summary = "Trae todos los comentarios en base a postId")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "200", description = "Se trae todos los comentarios filtrado por postId con exito", 
+	    content = { @Content(mediaType = "application/json", 
+	    schema = @Schema(implementation = CommentDto.class)) }),
+	@ApiResponse(responseCode = "404", description = "Los comentarios no fueron encontradas en el servicio web", content = @Content)})
 	@GetMapping("/comment/{id}")
 	public ResponseEntity<Object> getcommentsByPostId(@PathVariable("id") int postId){	
 		try {	
-			return ResponseEntity.status(HttpStatus.CREATED).body(commentsService.getCommentsByPostId(postId));
+			return ResponseEntity.ok().body(commentsService.getCommentsByPostId(postId));
 		}catch(Exception e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error en la busqueda de datos, " + e.getMessage());
 		}
@@ -90,7 +141,11 @@ public class BusinessController {
 		
 	/*************** Permisos ********************/
 	
-	//crea un permiso, solo el titular del album
+	@Operation(summary = "Crea un permiso")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "201", description = "Permiso creado con exito", content = @Content),
+	@ApiResponse(responseCode = "400", description = "El permiso no se pudo crear por problemas en la inserccion a la base de datos", 
+	    		content = @Content) })
 	@PostMapping("/newpermission/{id}")
 	public ResponseEntity<Object> newPermission (@RequestBody PermissionDto permissionDto, @PathVariable("id") int userId){	
 		try {	
@@ -101,29 +156,43 @@ public class BusinessController {
 		}
 	}
 	
-	//actualiza el tipo de permiso, solo el titular del album
+	@Operation(summary = "Actualiza un permiso")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "201", description = "Permiso ha sido actualizado con exito", content = @Content),
+	@ApiResponse(responseCode = "400", description = "El permiso no se pudo crear por problemas en la inserccion a la base de datos", 
+	    		content = @Content) })
+	//solo el titular del album
 	@PutMapping("/updatepermission/{id}")
 	public ResponseEntity<Object> updatePermission (@RequestBody PermissionDto permissionDto, @PathVariable("id") int userId){	
 		try {	
 			permissionsService.update(permissionDto, userId);
-			return ResponseEntity.status(HttpStatus.CREATED).body("Permiso ha sido actualizado correctamente");
+			return ResponseEntity.ok().body("Permiso ha sido actualizado correctamente");
 		}catch(Exception e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al actualizar el permiso, " + e.getMessage());
 		}
 	}
 	
-	//retorna lista de usuarios de un album especifico y un tipo de permiso
+	@Operation(summary = "Trae todos los usuarios de un album especifico y un tipo de permiso")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "200", description = "Se trae todos los usuarios filtrado por album y permiso con exito", 
+	    content = { @Content(mediaType = "application/json", 
+	    schema = @Schema(implementation = UserDto.class)) }),
+	@ApiResponse(responseCode = "404", description = "Los usuarios no fueron encontrados", content = @Content)})
 	@GetMapping("/usersandpermissions/album={idalbum}/permission={idpermission}")
 	public ResponseEntity<Object> getUsersByAlbumAndPermission (@PathVariable("idalbum") int idAlbum, @PathVariable("idpermission") int idPermission){	
 		try {	
-			return ResponseEntity.status(HttpStatus.CREATED).body(permissionsService.getUsersByAlbumAndPermission(idAlbum, idPermission));
+			return ResponseEntity.ok().body(permissionsService.getUsersByAlbumAndPermission(idAlbum, idPermission));
 		}catch(Exception e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error en la busqueda de datos, " + e.getMessage());
 		}
 	}
 	
-	
-	//retorna un album si es que el usuario tiene permisos
+	@Operation(summary = "Trae un album si es que el usuario tiene permisos")
+	@ApiResponses(value = { 
+	@ApiResponse(responseCode = "200", description = "Se trae un album con exito", 
+	    content = { @Content(mediaType = "application/json", 
+	    schema = @Schema(implementation = UserDto.class)) }),
+	@ApiResponse(responseCode = "404", description = "Error en la busqueda de los datos o no hay permisos", content = @Content)})
 	@GetMapping("/album={idalbum}/user={iduser}")
 	public ResponseEntity<Object> getByIdwithPermission(@PathVariable("idalbum") int idAlbum, @PathVariable("iduser") int userId){
 		try {	
@@ -136,7 +205,7 @@ public class BusinessController {
 			//verifico que tenga los permisos o que sea el dueño del album
 			if(permission == 0 && album.getUserId() != userId) throw new Exception("El usuario no tiene permiso de ver el album");
 			
-			return ResponseEntity.status(HttpStatus.CREATED).body(album);
+			return ResponseEntity.ok().body(album);
 		}catch(Exception e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error en la busqueda de datos, " + e.getMessage());
 		}
